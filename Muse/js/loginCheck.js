6b4f8871d82e7009c3cd8117ec8d64d74cecdee0
IsakMalm <113511794+IsakMalm@users.noreply.github.com>
2023-06-01 12:41:26 +0200

    Delete Muse directory

--- Muse/js/loginCheck.js
+++
@@ -1,7 +0,0 @@
-window.onload = function checkLogin(){
-    if(sessionStorage.getItem("login") === null){
-        console.log("not logged in");
-    } else {
-        console.log("already logged in");
-    }
-}
